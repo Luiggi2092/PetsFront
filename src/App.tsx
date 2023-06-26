f6e8b64680ead354588a5ea33797be9e146f5a9e
Magui Martin <Magali Martin>
2023-06-26 18:38:45 -0300

    ruta registro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Landing from './Pages/Landing/Landing'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Registro from './Pages/registro/Registrate'
 
 function App() {
   return (   
     <div className='App'>
     <Routes>
       <Route path = "/" element={<Landing/>}/>
+      <Route path = "/registro" element={<Registro/>}/>
     </Routes>
     </div>
   )
