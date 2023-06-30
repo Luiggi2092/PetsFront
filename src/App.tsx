3b8a9e5644bf18ff7fe0907c02afefe2d3558261
David Perez Tiburcio <sewer7375@gmail.com>
2023-06-30 16:23:43 -0600

    login-view

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ import Navbar from './components/navbar/Navbar';
 import Home from './Pages/home/Home';
 import UneteANosotros from './Pages/nosotros/UneteANosotros';
 import PetShop from './Pages/Tienda/PetShop';
-import InicioSesion from './Pages/inicio/InicioSesion';
 import Registrate from './Pages/registro/FormRegistrate';
 import SocialIcons from './Pages/socialIcons/SocialIcons';
 import Landing from './Pages/Landing/Landing'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PetDetail from '../src/Pages/PetDetail/PetDetail';
+import Login from './Pages/inicio/InicioSesion';
 
 
 const App: React.FC = () => {
@@ -25,7 +25,7 @@ const App: React.FC = () => {
                 <Route path="/home" element={<Home />} />
                 <Route path="/nosotros" element={<UneteANosotros />} />
                 <Route path="/petshop" element={<PetShop />} />
-                <Route path="/inicio-sesion" element={<InicioSesion />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/registrato" element={<Registrate />} />
                 <Route path="/pet/:id" element={<Registrate />} />
                 <Route path="/pets/:id" element={<PetDetail/>} />
--- src/pages/inicio/InicioSesion.tsx
+++ src/pages/inicio/InicioSesion.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './inicioSesion.css'
+import { useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import style from './inicioSesion.module.css'
 
-const InicioSesion = () => {
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = () => {
+    axios
+      .post('/api/users', { email, password })
+      .then(response => {
+        if (response.status === 200) {
+          loginWithRedirect();
+        } else {
+          console.error('Error en el inicio de sesión');
+        }
+      })
+      .catch(error => {
+        console.error('Error en el inicio de sesión', error);
+      });
+  };
+
+  const handleGoogleLogin = () => {
+    loginWithRedirect({connection: 'google-oauth2'});
+  }
+
   return (
-    <div>InicioSesion</div>
+    <div className={style.containerForm}>
+      <h2>Inicio de sesión</h2>
+      <input type='email' placeholder='ejemplo@hotmail.com' value={email} onChange={e => setEmail(e.target.value)} />
+      <input type='password' placeholder='Contraseña' value={password} onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Iniciar sesión</button>
+      <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
+      <p>¿No tienes cuenta?</p> <Link to='/registro'><button>Registrate</button></Link>
+    </div>
   )
 }
 
-export default InicioSesion
+export default Login;
