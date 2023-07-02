8732027a53741f867a04136d1f53acf853aad0fb
e-commerceTechComputers <85269071+e-commerceTechComputers@users.noreply.github.com>
2023-07-02 08:48:27 -0500

    Login commit

--- src/pages/inicio/InicioSesion.tsx
+++ src/pages/inicio/InicioSesion.tsx
@@ -1,20 +1,62 @@
 import { useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import style from './inicioSesion.module.css'
+import {useEffect} from "react"
+import jwt_decode from "jwt-decode";
+
+declare const google: any;
+
 
 const Login = () => {
+
+   const navigate = useNavigate();
+  
+  interface user1 {
+    name: string | null;
+    iat?:number,
+    iss?:string,
+    picture?:string
+  }
+
+
+  const [user,setUser] = useState<any>({})
+
+ 
+   function handleCallbackResponse(response:any){
+       console.log("Enconded JWT ID token" + response.credential)
+
+       var userObject = jwt_decode(response.credential);
+       console.log(userObject)
+       setUser(userObject)
+      
+
+   }
+
+
+  useEffect(()=> {
+         // global google
+         google.accounts.id.initialize({
+               client_id:"455768951489-dpmia14fe22vcrimo4fmgbtqnngab2b7.apps.googleusercontent.com",
+               callback: handleCallbackResponse
+         })
+         google.accounts.id.renderButton(
+           document.getElementById("signInDiv"),
+          {theme: "outline", size: "large"}
+         )
+
+         google.accounts.id.prompt();
+  },[])
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { loginWithRedirect } = useAuth0();
 
   const handleLogin = () => {
     axios
       .post('/api/users', { email, password })
       .then(response => {
         if (response.status === 200) {
-          loginWithRedirect();
+         // loginWithRedirect();
         } else {
           console.error('Error en el inicio de sesión');
         }
@@ -24,9 +66,17 @@ const Login = () => {
       });
   };
 
-  const handleGoogleLogin = () => {
-    loginWithRedirect({connection: 'google-oauth2'});
+  // const handleGoogleLogin = () => {
+  //   loginWithRedirect({connection: 'google-oauth2'});
+  // }
+
+  const handleHome = () => {
+
+     navigate('/');
+        
   }
+
+
 
   return (
     <div className={style.containerForm}>
@@ -34,7 +84,15 @@ const Login = () => {
       <input type='email' placeholder='ejemplo@hotmail.com' value={email} onChange={e => setEmail(e.target.value)} />
       <input type='password' placeholder='Contraseña' value={password} onChange={e => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Iniciar sesión</button>
-      <button onClick={handleGoogleLogin}>Iniciar sesión con Google</button>
+{      
+      <div id="signInDiv" onClick={handleHome} >
+          
+      </div>}
+      <div className={user? "profile":"hidden"}>
+        {user.picture && <Link to={'/home'}>
+        <img  src={user.picture} alt=""/>
+        </Link>}
+      </div>
       <p>¿No tienes cuenta?</p> <Link to='/registro'><button>Registrate</button></Link>
     </div>
   )
