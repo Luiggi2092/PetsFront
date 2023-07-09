697a1984df72bc80796701d509a72e618da7828f
Magui Martin <Magali Martin>
2023-07-09 19:26:30 -0300

    magui: estilos

--- src/pages/registro/Registrate.tsx
+++ src/pages/registro/Registrate.tsx
@@ -60,8 +60,6 @@ const Registro = () => {
   }, [dispatch])
 
   const usersType = useSelector((state:any) => state.UsersType)
-  
-  //const [userTypes, setUserTypes] = useState<string[]>([])
 
   const handlerChange = (event: ChangeEvent<HTMLInputElement>) => {
     const property = event.target.name
@@ -90,17 +88,21 @@ const Registro = () => {
       error.type = 'Debe seleccionar un tipo de usuario'
     } else error.type = ''
 
-    if (!input.name.match(/^[a-zA-Z_]+([a-zA-Z_]+)*$/)) {
-      error.name = 'Solo se permiten letras y no debe haber espacios al final.'
+    if (!input.name.match(/^[A-ZÑa-zñáéíóúÁÉÍÓÚ'° ]+$/)) {
+      error.name = 'No se permiten números ni símbolos de puntuación.'
     } else error.name = ''
 
 /*     if (!input.surname.match(/^[a-zA-Z_]+([a-zA-Z_]+)*$/)) {
       error.surname = 'Solo se permiten letras y no debe haber espacios al final.'
     } else error.surname = '' */
 
-    if (!input.email.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/)) {
+    if (!input.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/)) {
       error.email = 'El correo es inválido.'
     } else error.email = ''
+
+    if (!input.passwordKey.match(/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/)) {
+      error.passwordKey = 'La contraseña debe tener unaa longitud total de al menos 8 caracteres, al menos un carácter especial, dos dígitos numéricos,  y al menos tres letras minúsculas.'
+    }
 
     return error
   }
@@ -131,24 +133,23 @@ const Registro = () => {
 
   return (
     <div className={style.container}>
-      <div>
         <form className={style.form}>
           <div >
             <div>
               <label htmlFor="type">Registrarme como: </label>
             </div>
             {usersType.map((userType: any) => (
-            <div key={userType.id}>
+            <div key={userType.id} className={style.users}>
               <input type="radio" value={userType.name} name="type" checked={form.type === userType.name} onChange={handlerChange}/>
-              <label htmlFor={userType.id}>{userType.name}</label>
+              <label htmlFor={userType.id} className={style.label} >{userType.name}</label>
             </div>
           ))}
           </div>
           {error.type && <p className={style.error}>{error.type}</p>}
 
-          <div>
-            <label htmlFor="name" >Nombre Completo: </label>
-            <input type="text" name="name" value={form.name} onChange={handlerChange} />
+          <div >
+            <label htmlFor="name" className={style.label}>Nombre Completo: </label>
+            <input type="text" name="name" value={form.name} onChange={handlerChange}className={style.input}/>
             {/* <label htmlFor="surname" className={style.label}>Apellido: </label>
             <input type="text" name="surname" value={form.surname} onChange={handlerChange}/> */}
           </div>
@@ -161,13 +162,13 @@ const Registro = () => {
           </div> */}
 
           <div>
-            <label htmlFor="phone">Teléfono: </label>
-            <input type="tel" name="phone" value={form.phone} onChange={handlerChange}/>
+            <label htmlFor="phone" className={style.label}>Teléfono: </label>
+            <input type="tel" name="phone" value={form.phone} onChange={handlerChange} className={style.input}/>
           </div>
 
-          <div>
-            <label htmlFor="address">Pais: </label>
-            <input type="text" name="address" value={form.address} onChange={handlerChange}/>
+          <div >
+            <label htmlFor="address" className={style.label}>Pais: </label>
+            <input type="text" name="address" value={form.address} onChange={handlerChange} className={style.input}/>
             {/* <label htmlFor="estado/provincia" className={style.label}>Estado/Provincia: </label>
             <input type="text" name="estadoProvincia" value={form.estadoProvincia} onChange={handlerChange}/>
             <div>
@@ -177,14 +178,14 @@ const Registro = () => {
           </div>
 
           <div>
-            <label htmlFor="email">Correo: </label>
-            <input type="text" name="email" value={form.email} onChange={handlerChange}/>
+            <label htmlFor="email" className={style.label}>Correo: </label>
+            <input type="text" name="email" value={form.email} onChange={handlerChange} className={style.input}/>
           </div>
           {error.email && <p className={style.error}>{error.email}</p>}
 
-          <div>
-            <label htmlFor="passwordKey">Contraseña: </label>
-            <input type="password" name="passwordKey" value={form.passwordKey} onChange={handlerChange} />
+          <div >
+            <label htmlFor="passwordKey" className={style.label}>Contraseña: </label>
+            <input type="password" name="passwordKey" value={form.passwordKey} onChange={handlerChange} className={style.input}/>
           </div>
 
           <button type="submit" onClick={handlerSubmit} className={style.boton}>
@@ -194,10 +195,9 @@ const Registro = () => {
           <h1 className={style.h1}>
             ¿Ya tienes cuenta?<Link to="/login">Iniciar Sesión</Link>
           </h1>
-        </form>
-      </div>
+        </form> 
     </div>
   )
-}
+} 
 
 export default Registro
