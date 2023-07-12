e2e0fdd0e3f5196099523aaff485930689add7f2
David Perez Tiburcio <sewer7375@gmail.com>
2023-07-12 13:38:36 -0600

    added Dashboard Forms

---
+++ src/components/AdoptionList/adoptionList.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import style from './adoptionList.module.css'
+
+interface Forms {
+    id: string,
+    name: string,
+    lastName: string,
+    email: string,
+    address: string,
+    pais: string,
+    phone: string,
+    economicSituation: string,
+    previousPetExperience: string,
+    petAllergy: string,
+    dailyPetTime: string,
+    estado: string,
+}
+
+const AdoptionFormList: React.FC = () => {
+    const [forms, setForms] = useState<Forms[]>([]);
+
+    useEffect(() => {
+        const fetchAdoptionsForms = async () => {
+            try {
+                const response = await axios.get<Forms[]>('/forms');
+                setForms(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchAdoptionsForms();
+    }, []);
+
+    const handleAceptar = async (id: string) => {
+        try {
+            await axios.get(`/forms/${id}/aceptar`);
+            //actualizar estado a aceptado
+            const updatedForms = forms.map((form) => {
+                if (form.id === id) {
+                    return { ...form, estado: 'aceptado' };
+                }
+                return form;
+            });
+            setForms(updatedForms);
+            alert('Formulario aceptado correctamente');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleRechazar = async (id: string) => {
+        try {
+            await axios.get(`/forms/${id}/rechazar`)
+            //actualizar estado a rechazado
+            const updatedForms = forms.map((form) => {
+                if (form.id === id) {
+                    return { ...form, estado: 'rechazado' };
+                }
+                return form;
+            });
+            setForms(updatedForms);
+            alert('Formulario rechazado');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <div>
+            <h2>Formularios de solicitud de adopción</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Dirección</th>
+                        <th>Pais</th>
+                        <th>Telefono</th>
+                        <th>Situación <br /> Económica</th>
+                        <th>Experiencia previa <br /> con mascotas</th>
+                        <th>Alergia a <br /> mascotas</th>
+                        <th>Tiempo a dedicar <br /> a la mascota</th>
+                        <th>Estado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forms.map((form) => (
+                        <tr key={form.id}>
+                            <td>{form.name} {form.lastName}</td>
+                            <td>{form.email}</td>
+                            <td>{form.address}</td>
+                            <td>{form.pais}</td>
+                            <td>{form.phone}</td>
+                            <td>{form.economicSituation}</td>
+                            <td>{form.previousPetExperience}</td>
+                            <td>{form.petAllergy}</td>
+                            <td>{form.dailyPetTime}</td>
+                            <td>{form.estado}</td>
+                            <td>
+                                {form.estado !== 'aceptado' && (
+                                    <div className={style.butonsForm}>
+                                        <button onClick={() => handleAceptar(form.id)}>Aceptar</button>
+                                        <button onClick={() => handleRechazar(form.id)}>Rechazar</button>
+                                    </div>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+};
+
+export default AdoptionFormList;
